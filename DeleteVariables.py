c4f56e8432be9c1472e8df440ded98f061bd0795
SheetalNagmode <sheetalnagmode@gmail.com>
2025-03-04 13:30:57 -0500

    3/4/25

---
+++ DeleteVariables.py
@@ -0,0 +1,6 @@
+a=100
+print(a)
+
+del a           #Deletes the variable
+
+print(a)        # NameError: name 'a' is not defined
---
+++ TakingInput.py
@@ -0,0 +1,26 @@
+num1=input("Enter the first number: ")
+num2=input("Enter second number: ")
+
+print(num1+num2)
+
+# Approach 1st:
+
+num1= int(input("Enter first number: "))
+num2= int(input("Enter second number: "))
+
+print(num1+num2)
+
+# Approach 2nd:
+
+num1= input("Enter first number: ")
+num2= input("Enter second number: ")
+
+print(int(num1)+int(num2))
+
+num1= float(input("Enter 1st decimal number: "))
+num2= float(input('Enter 2nd decimal number: '))
+print(num1+num2)
+
+num1= input("Enter 1st decimal number: ")
+num2= input('Enter 2nd decimal number: ')
+print(float(num1)+ float(num2))
