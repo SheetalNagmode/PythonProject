016c87227cf7ea92d715e78bfceb19503430b8b8
SheetalNagmode <sheetalnagmode@gmail.com>
2025-03-05 14:30:20 -0500

    3/5/25

--- Comments.py
+++ Comments.py
@@ -1,10 +1,10 @@
-''' This is an example of
-explaining comments in python'''
+
 """This is an example of 
- explaining comments in python'''
-#  use ctrl+/ for #
+ explaining comments in python"""
+#  use ctrl+/ for
 
 x=100           #Assigning 100 to x
 y=200           #Assigning 200 to y
-print(x+y)      # for single comment and ''' or """ for multiline comments
+print(x+y)
+# for single comment and ''' or """ for multiline comment
 
---
+++ ConditionalStatements.py
@@ -0,0 +1,38 @@
+#Conditional statements: if...else and elif
+#Syntax :
+#if condition:
+#   statements
+#else:
+#   statements
+# indentation before the statements is very important and must
+# No semicolon or comma needed at the end of statement.
+# Colon at the end of if and else is needed.
+# Multiple statements can be added for both.
+# Either condition can be added after if or else or boolean values can be used(True, False)
+# Comma is used inside the statement if needed to append any value.
+# In python True= 1 and False= 0.
+# Instead of true or false 1 or 0 can be used respectively
+
+# Example 1:
+
+a=50
+if a>30:
+    print("This is a true condition")
+else:
+    print("This is a false condition")
+
+# Example 2:
+
+if False:
+    print('This is a true condition')
+else:
+    print('This is a false condition')
+
+#Example 3:
+if 0:
+    print('This is a true condition')
+else:
+    print('This is a false condition')
+
+
+
