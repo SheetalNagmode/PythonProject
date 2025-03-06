9377bb07ad559a69c96aad78b13f90430608bac9
SheetalNagmode <sheetalnagmode@gmail.com>
2025-03-06 12:25:08 -0500

    3/6/25

---
+++ StringConvertingFunctions.py
@@ -0,0 +1,30 @@
+# Converting String Functions:
+# capitalize()  = Capitalizes the string(1st letter of the first word is capitalized )
+# title() = Every word in the sentence will start with capital letter
+# lower() = All the characters in the string will be converted to lowercase
+# upper() = All the characters in the string will be converted to uppercase
+# swapcase() = will convert upper into lower case and vice versa
+# replace("","") = replace single or multiple characters in a string with suggested new characters
+# Examples of the functions:
+
+s="String in PYTHON"
+
+s1= s.capitalize()
+print(s1)
+
+s2 = s.title()
+print(s2)
+
+s3 = s.lower()
+print(s3)
+
+s4 = s.upper()
+print(s4)
+
+s5 = s.swapcase()
+print(s5)
+
+s6 = s.replace("in","on")
+print(s6)
+
+print(s)
