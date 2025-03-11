4556b486c18d51832ea1d1c4f980668b1d93fba2
SheetalNagmode <sheetalnagmode@gmail.com>
2025-03-11 13:21:57 -0400

    3/11/25

--- GlobalVariablesVsLocalVariables.py
+++ GlobalVariablesVsLocalVariables.py
@@ -17,3 +17,51 @@ func()
 
 print(global_var)
 #print(local_var)  # not valid bcoz this is local variable inside the function
+
+# Global variable behaviour within and outside the function:
+# If changes are made to the global variable within the function,
+# it will only reflect within the function.
+# It will not affect the original value of the global variable outside the function.
+
+
+xy=100
+
+def cool():
+    xy=200
+    print(xy)
+
+cool()
+print(xy)
+
+
+# Changing Global variable to local variable:
+# in this case the global variable changed within the function
+# will also reflect outside the function
+
+
+t=1
+
+def increment():
+    #global t=100 # invalid
+    global t
+    t=100
+    print(t)
+
+increment()
+
+print(t)
+
+
+# There is no need to declare global variable outside the function
+# you can declare them global inside the function
+
+def foo():
+    global x
+    x=100
+    print(x)
+
+
+foo()
+
+print(x)    #trying to print global variable outside of function
+
