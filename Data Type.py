b56b0333117e6f4d927899b1b5fadb561f68a8c4
SheetalNagmode <sheetalnagmode@gmail.com>
2025-06-03 15:48:30 -0400

    6/3/25

--- Data Type.py
+++ Data Type.py
@@ -2,7 +2,7 @@ x=100           #Number - int
 y=10.5          #Number-float
 s1="Welcome"    #String/char
 s2="welcome123" #String/char
-
+c='A'           # char/String both can be in single or double quotation
 b=True          #Boolean
 
 print(type(x))      # 'type()'is a method to know the data type of the variables
@@ -10,5 +10,7 @@ print(type(y))
 print(type(s1))
 print(type(s2))
 print(type(b))
+print(type(c))
+
 
 
