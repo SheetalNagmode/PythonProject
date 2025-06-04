0883ec6f840f31ebd104f9e820a96827d5e53bed
SheetalNagmode <sheetalnagmode@gmail.com>
2025-06-04 08:17:06 -0400

    6/4/25

--- Day3/Range()Function.py
+++ Day3/Range()Function.py
@@ -1,17 +1,29 @@
-# Range()
+# Range()  :  values between the range
 # for a single parameter it will start default from zero
 # and end in -1 of the maximum parameter.
-# If first parameter/argument is mentioned it will start from the specified number and end -1 of the last number.
-# if three parameters are provided it will follow the same for the first two numbers as above
+# If first parameter/argument is mentioned
+# it will start from the specified number and end -1 of the last number.
+# if three parameters are provided
+# it will follow the same for the first two numbers as above
 # but third parameter will be either incrementation or decremental
 #
 print(range(10))                # range(0, 10)
 print(list(range(10)))          #[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 print(list(range(5,10)))        #[5, 6, 7, 8, 9]
 
+# print only odd numbers between 1 - 10
 print(list(range(1,10,2)))      #[1, 3, 5, 7, 9]
+
+# print only even numbers between 0 - 10
 print(list(range(0,10,2)))      #[0, 2, 4, 6, 8]
+print(list(range(2,10,2)))      #[0, 2, 4, 6, 8]
+
+# print numbers 1 to 10 in descending order
 print(list(range(10,1,-1)))     #[10, 9, 8, 7, 6, 5, 4, 3, 2]
+
+print(list(range(-10, -5)))     #[-10, -9, -8, -7, -6]
+print(list(range(-10, -5, 2)))  #[-10, -8, -6]
+
 
 for x in range(10):
     print(x)
--- Day3/WhileLoop.py
+++ Day3/WhileLoop.py
@@ -6,17 +6,21 @@
 #       statements;
 #       i=i+1
 
+# Print number 1 to 10 in ascending order
 # Incrementation Example:
 
 i=1;
 while i<=10:
     print(i)
     i=i+1
+print("Done!!")
 
 # if range is known use "for loop" otherwise use "while loop"
+# Print number 10 to 1 in descending order.
 # Decrementation Example:
 
 i=10;
 while i>=1:
     print(i)
     i=i-1
+print("Done!!")
