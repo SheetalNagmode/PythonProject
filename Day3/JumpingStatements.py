c34e5e0fd7e57d669ec226607044ba33835e66ec
SheetalNagmode <sheetalnagmode@gmail.com>
2025-06-04 12:01:16 -0400

    6/4/25

--- Day3/JumpingStatements.py
+++ Day3/JumpingStatements.py
@@ -9,13 +9,23 @@ for i in range(1,10):
     if i==5:
         break
     print(i)
-
 print("Program Exited")
+
+# Print 5 to 100 with 5 increment:
+for i in range(5, 101, 5):
+     print(i)
 
 #Continue Example:
 for i in range(1,10):
     if i==5:
         continue
     print(i)
+print("Program Exited")
 
+# skip with multiple values using 'or' keyword:
+
+for i in range(1,10):
+    if i==3 or i==5 or i==7:
+        continue
+    print(i)
 print("Program Exited")
