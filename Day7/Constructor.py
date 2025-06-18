d8665e3b56052655be59271162e154a6f7e2c01d
SheetalNagmode <sheetalnagmode@gmail.com>
2025-06-18 15:30:39 -0400

    6/18/25

--- Day7/Constructor.py
+++ Day7/Constructor.py
@@ -25,4 +25,38 @@ mc=MyClass("David")    # Passing parameter to the constructor
 
 
 # Example 3:
+# requirements: Employee class
+# constructor: eid, ename, sal
+# diplay(): print eid, ename & sal
 
+class Emp:
+
+    def __init__(self, eid, ename, sal):
+        self.eid=eid
+        self.ename=ename
+        self.sal=sal
+    def display(self):
+        print(self.eid, self.ename,self.sal)
+
+e1=Emp(101, "John", 120000)
+e1.display()
+
+e2=Emp(102,"Scott",80000)
+e2.display()
+
+# Example 4:
+# requirements: Employee class
+# constructor: eid, ename, sal
+# constructor : print eid, ename & sal
+
+class Emp:
+
+    def __init__(self, eid,ename,sal):
+        self.eid = eid
+        self.ename = ename
+        self.sal = sal
+    def __str__(self):      # returns only a string
+        return (self.ename)
+
+e1=Emp(101, "John", 120000)
+print(e1)
