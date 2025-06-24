aa74c5df81961b1b79cc857b4840cde149dd444e
SheetalNagmode <sheetalnagmode@gmail.com>
2025-06-24 13:08:27 -0400

    6/24/25

--- Day9/PackC/Client.py
+++ Day9/PackC/Client.py
@@ -9,3 +9,14 @@ empobj.displayemp()
 import stu
 stuobj=stu.Student(111, "Scott", "A")
 stuobj.displaystu()
+
+
+# Approach 2:
+
+from emp import Employee
+empobj=Employee(101, "John", 120000)
+empobj.displayemp()
+
+from stu import Student
+stuobj=stu.Student(111, "Scott", "A")
+stuobj.displaystu()
