1b5901c3762d2412110ccffffb15a13708d11150
SheetalNagmode <sheetalnagmode@gmail.com>
2025-06-25 14:50:45 -0400

    6/25/25

---
+++ Day10/Exceptions.py
@@ -0,0 +1,13 @@
+
+print("This is starting point of program")
+print("This is starting point of program")
+print("This is starting point of program")
+print("This is starting point of program")
+try:
+    print(x)
+except:
+    print("Exception occured")
+
+print("This is end of the program")
+print("This is end of the program")
+print("This is end of the program")
