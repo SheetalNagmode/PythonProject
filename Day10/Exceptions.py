fd7fe0da119b9fc59051ba5723b7247568607468
SheetalNagmode <sheetalnagmode@gmail.com>
2025-06-25 15:22:46 -0400

    6/25/25

--- Day10/Exceptions.py
+++ Day10/Exceptions.py
@@ -22,3 +22,39 @@ except:
     print("Exception occurred... Handled..")
 
 print("Program completed...")
+
+# Example 3: Multiple except blocks : try, except, else, finally
+
+try:
+    num1, num2 = 10,0
+    result=num1/num2
+    print("result is:", result)
+except ZeroDivisionError:
+    print("Thrown ZeroDiviisonError exception..")
+except SyntaxError:
+    print("Thrown Syntax error exception...")
+except:
+    print("exception handled...")
+else:
+    print("No exceptions occurred..")
+finally:
+    print("Always execute...")
+
+
+# Example 4: raising our own exception
+
+def enterage(num):
+    if num<0:
+        raise ValueError("Only Integers are allowed")
+    if num%2==0:
+        print("Even")
+    else:
+        print("Odd")
+
+print("Checking number is even or odd by callng function..")
+num=-1
+try:
+    enterage(num)
+except ValueError:
+    print("Value error exception occurred and handled..")
+print("Program completed..")
