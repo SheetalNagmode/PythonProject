ef90715cc6cbb8de19d9ccc03be33c7a43d6cec6
SheetalNagmode <sheetalnagmode@gmail.com>
2025-06-25 14:58:03 -0400

    6/25/25

--- Day10/Exceptions.py
+++ Day10/Exceptions.py
@@ -1,4 +1,4 @@
-
+# Example 1:
 print("This is starting point of program")
 print("This is starting point of program")
 print("This is starting point of program")
@@ -11,3 +11,14 @@ except:
 print("This is end of the program")
 print("This is end of the program")
 print("This is end of the program")
+
+# Example 2:
+
+print("This is starting of the program...")
+print("Program in progress")
+try:
+    print(10/0)
+except:
+    print("Exception occurred... Handled..")
+
+print("Program completed...")
