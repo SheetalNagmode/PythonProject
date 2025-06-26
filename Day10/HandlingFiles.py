1eaf1a7808d2df44a90968dc1cd5a5bea96b4b8b
SheetalNagmode <sheetalnagmode@gmail.com>
2025-06-26 13:00:06 -0400

    6/26/25

--- Day10/HandlingFiles.py
+++ Day10/HandlingFiles.py
@@ -8,3 +8,19 @@ file.write("This is my fourth statement...\n")
 file.write("This is my fifth statement...\n")
 file.close()
 print("Program Completed...")
+
+# Example 2:  Reading data from text file
+
+file=open("C:/Demofiles/myfiles.txt", 'r')
+# print(file.read())      # reads whole data
+print(file.readline())    # read only the first line
+file.close()
+
+# example 3: Appending data into the text file
+
+file=open("C:/Demofiles/myfiles.txt", 'a')
+file.write("This is my sixth line...\n")
+file.write("This is my seventh line...\n")
+file.close()
+print("Program is Completed...")
+
