c4b6166378616c7e4e8db4d7ecde35941ce5b9e6
SheetalNagmode <sheetalnagmode@gmail.com>
2025-08-24 00:35:55 -0400

    8/24/25

--- Day 11/FirstTestCase.py
+++ Day 11/FirstTestCase.py
@@ -12,13 +12,34 @@
 import time
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 service= Service(r"C:\Drivers\chromedriver-win64\chromedriver.exe")
 driver = webdriver.Chrome(service=service)
+
+driver.implicitly_wait(10)    # wait max 10 sec for elements
+
 driver.get("https://opensource-demo.orangehrmlive.com/")
 
-time.sleep(5)  # Keeps browser open for 5 seconds
+# time.sleep(1)  # Keeps browser open for 10 seconds
+
+driver.find_element(By.NAME, "username").send_keys("Admin")
+driver.find_element(By.NAME, "password").send_keys("admin123")
+driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+
+act_title = driver.title
+exp_title = "OrangeHRM"
+
+if act_title==exp_title:
+    print("Login Test Passed")
+else:
+    print("Login Test Failed")
+
+time.sleep(2)
+
+driver.close()
 
 
 
