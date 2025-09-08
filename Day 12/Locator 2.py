024223eadc2bfcc3437106609c9f981864e51f29
SheetalNagmode <sheetalnagmode@gmail.com>
2025-09-08 14:58:13 -0400

    9/8/25

---
+++ Day 12/Locator 2.py
@@ -0,0 +1,13 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+serv_obj=Service(r"C:\Drivers\chromedriver-win64\chromedriver.exe")
+driver=webdriver.Chrome(service= serv_obj)
+
+driver.get("http://automationpractice.com/index.php")
+driver.maximize_window()
+
+sliders= driver.find_elements(By.CLASS_NAME, "homeslider-container")
+print(len(sliders))
